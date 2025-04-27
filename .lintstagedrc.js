842e996814987b6543d30d499e71614ae4273a1d
AhmedShehab1 <legendbtw2003@gmail.com>
2025-04-27 22:10:35 +0300

    Mistake

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,6 @@
 module.exports = {
-    // Lint & format JavaScript files
-    "*.{js,jsx}": [
-      "eslint --fix",
-      "prettier --write"
-    ],
-    // Format other file types
-    "*.{json,css,md}": [
-      "prettier --write"
-    ]
+    "*.js": "eslint --fix",
+    "*.jsx": "eslint --fix",
+    "*.{json,css,md}": "prettier --write",
   };
   
