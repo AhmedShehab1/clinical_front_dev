f045f1324a9ae0a6f031bd4ac52b7ccf1a89a6ed
AhmedShehab1 <legendbtw2003@gmail.com>
2025-05-01 18:32:58 +0300

    Add routing and login page to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Container from "react-bootstrap/Container";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+
 export default function App() {
   return (
-    <>
+    <Container fluid className="App">
       <h1>Clinical System</h1>
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
